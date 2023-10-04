6d1aadde08feb68503c161602217f07201c59272
Joshua Gaviola <martin100906@gmail.com>
2023-10-03 19:33:33 -0700

    10/3/2023 10:33 PM

---
+++ 49.) Group Anagrams/solution.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            anagramGroups.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(anagramGroups.values());
+    }
+}
