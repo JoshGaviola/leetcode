cb36b0482fa787a46006023be7f56e126cd65b97
Joshua Gaviola <martin100906@gmail.com>
2023-10-05 03:23:30 -0700

    6:23 AM

---
+++ 59.) Spiral Matrix II/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1; // Initialize the starting number to 1
+        
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        
+        while (left <= right && top <= bottom) {
+            // Fill the top row
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+            
+            // Fill the rightmost column
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+            
+            // Fill the bottom row (if applicable)
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+            
+            // Fill the leftmost column (if applicable)
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        
+        return matrix;
+    }
+}
