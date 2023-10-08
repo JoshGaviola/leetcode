eaa1ea1665eb0dd4f15da6b67c88e00a864a83de
Joshua Gaviola <martin100906@gmail.com>
2023-10-08 01:50:09 -0700

    4:50 AM

---
+++ 79.) Word Search/solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true; // All characters in the word have been matched.
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word.charAt(index)) {
+            return false; // Out of bounds or mismatched character.
+        }
+
+        char originalChar = board[i][j];
+        board[i][j] = ' '; // Mark the cell as visited.
+
+        boolean found = dfs(board, word, i + 1, j, index + 1) ||
+                        dfs(board, word, i - 1, j, index + 1) ||
+                        dfs(board, word, i, j + 1, index + 1) ||
+                        dfs(board, word, i, j - 1, index + 1);
+
+        board[i][j] = originalChar; // Restore the cell.
+
+        return found;
+    }
+}
