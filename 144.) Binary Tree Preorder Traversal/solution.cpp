2295145bc07c038b6a4b9fd4c2f4bdbf69cd7bd8
Joshua Gaviola <martin100906@gmail.com>
2023-10-28 23:24:50 -0700

    solved

---
+++ 144.) Binary Tree Preorder Traversal/solution.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <stack>
+
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> nodeStack;
+
+        if (!root) {
+            return result;
+        }
+
+        nodeStack.push(root);
+
+        while (!nodeStack.empty()) {
+            TreeNode* current = nodeStack.top();
+            nodeStack.pop();
+            result.push_back(current->val);
+
+            if (current->right) {
+                nodeStack.push(current->right);
+            }
+
+            if (current->left) {
+                nodeStack.push(current->left);
+            }
+        }
+
+        return result;
+    }
+};
