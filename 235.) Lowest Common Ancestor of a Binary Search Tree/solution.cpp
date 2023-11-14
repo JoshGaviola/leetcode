8794efe8ceed854baa684e2bbd81e01e2718fc16
Joshua Gaviola <martin100906@gmail.com>
2023-11-14 04:02:37 -0800

    solved

---
+++ 235.) Lowest Common Ancestor of a Binary Search Tree/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) {
+            return nullptr;
+        }
+
+        // If both nodes are smaller than the root, LCA is in the left subtree
+        if (p->val < root->val && q->val < root->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        // If both nodes are larger than the root, LCA is in the right subtree
+        else if (p->val > root->val && q->val > root->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        // If one node is smaller and the other is larger, the current root is the LCA
+        else {
+            return root;
+        }
+    }
+};
