930d60e74821cc8d3251d5b8c69c422e7e5e6b43
Joshua Gaviola <martin100906@gmail.com>
2023-11-14 03:54:36 -0800

    solved

---
+++ 234.) Palindrome Linked List/solution.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) {
+            return true;
+        }
+
+        // Step 1: Find the middle of the linked list
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse the second half of the linked list
+        ListNode *prev = nullptr, *current = slow, *next;
+        while (current) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        // Step 3: Compare the first half and the reversed second half
+        ListNode *firstHalf = head, *secondHalf = prev;
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val) {
+                return false;
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return true;
+    }
+};
