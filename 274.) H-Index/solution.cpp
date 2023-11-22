3651dea4c744f600259bf8f217cb9f57e09ae333
Joshua Gaviola <martin100906@gmail.com>
2023-11-21 19:40:17 -0800

    solved

---
+++ 274.) H-Index/solution.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+
+        int h = 0;
+        while (h < citations.size() && citations[h] >= h + 1) {
+            ++h;
+        }
+
+        return h;
+    }
+};
